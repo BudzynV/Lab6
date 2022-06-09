e2e8f888aff22a0cd54215f4402cfb24dbdcc116
BudzynV <oskar.budzynski99@gmail.com>
2022-06-09 19:36:41 +0200

    Drugie zasilenie(zad do 4)

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestConnection/java/MyDB.java
@@ -0,0 +1,13 @@
+package pl.lublin.wsei.java.cwiczenia.test.TestConnection.java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyDB {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
